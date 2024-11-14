23fc8ad0450cbe1530fdff00c597dc12591f5266
PiaSR <pia@richling.at>
2024-11-14 14:42:27 +0200

    update dist

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',  
+  base: '/', 
   plugins: [react()],
   build: {
-    outDir: 'dist',  
+    outDir: 'dist', 
+    rollupOptions: {
+      input: 'index.html', 
+    },
   },
 });
