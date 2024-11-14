e79c04dc16e903900f7bc8c7fcf2af7b9a9275d0
PiaSR <pia@richling.at>
2024-11-14 16:46:14 +0200

    bubble width

--- src/components/SpeechBubbleEmail.jsx
+++ src/components/SpeechBubbleEmail.jsx
@@ -8,7 +8,7 @@ const SpeechBubbleEmail = ({className}) => {
 <path d="M12.1362 51.1492C12.1362 47.5834 6.74448 40.2993 1.97295 34.6743C0.686566 33.1579 2.10068 30.7651 4.03452 31.2284L30.3582 37.535C30.6941 37.6155 31.0486 37.6067 31.3805 37.5111C138.853 6.58323 220.809 -22.4621 248.658 29.6629C282.029 92.1226 88.2447 111.61 44.9214 103.115C-1.89799 93.9353 12.1362 57.1454 12.1362 51.1492Z" strokeWidth="2"/>
 </svg>
 
-				<span className='bubble-text'>wanna go manual?<br></br>
+				<span className='bubble-text'>wanna go manual?
 piasmithrichling@gmail.com</span>
 
 			</div>
